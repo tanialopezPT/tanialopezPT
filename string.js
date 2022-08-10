92af4e3dd1275ec79a714852d75902f436d1eac0
tanialopezPT <taniadag5@gmail.com>
2022-08-10 11:21:43 -0700

    string sum

---
+++ string.js
@@ -0,0 +1,11 @@
+
+
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+
+
+
+
+function sumStr(a,b) {
+    return String(Number(a)+Number(b));
+  }
